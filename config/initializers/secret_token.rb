bdda6763f342f13d46f03f3701e92e19ab19c52d
AndrewNg <andrew@chesscademy.com>
2014-02-19 17:43:12 -0500

    real first commit with naming and pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IFTTTProject::Application.config.secret_key_base = 'dfe2bf0dc8c7b742c075bf5afe506a6ba589f8e7b14a955368d167f8d891972dfb11644d3a30b366305f1dbebc2d489add7c401745edd7a36158f720b04c7ec9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IFTTTProject::Application.config.session_store :cookie_store, key: '_IFTTT_project_session'
