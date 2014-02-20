a6c1c84a7588354aa7d5027ce160c9d6df458fa9
Sabar Dasgupta <sabar@orchive.com>
2014-02-19 20:31:18 -0500

    blah

---
+++ app/assets/javascripts/home/index.js
@@ -0,0 +1,3 @@
+$("#submit_user").click(function(){
+  alert("The paragraph was clicked.");
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+  	@users = User.all
+  	@teams = Team.all
+  end
+
+  def pair_users
+
   end
 end
